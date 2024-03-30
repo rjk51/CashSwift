6d09f97ce56d44d65401f012890e65d038e42b01
rjk51 <ayaanrjk2004@gmail.com>
2024-03-30 23:06:37 +0530

    Ui improvements in transac history

--- lib/pages/transactions.dart
+++ lib/pages/transactions.dart
@@ -13,12 +13,15 @@ class TransactionScreen extends StatelessWidget {
       appBar: CustomAppBar(),
       body: Column(
         children: [
-          Text('Previous Transactions: ', style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Theme.of(context).colorScheme.primary
-          ),),
-          const SizedBox(height: 20,),
+          Text(
+            'Previous Transactions: ',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          const SizedBox(height: 20),
           FutureBuilder<QuerySnapshot>(
             future: FirebaseFirestore.instance
                 .collection('transactions')
@@ -36,36 +39,50 @@ class TransactionScreen extends StatelessWidget {
                 );
               } else {
                 final List<DocumentSnapshot> transactions = snapshot.data!.docs;
-          
+
                 return SingleChildScrollView(
                   child: SizedBox(
                     height: 400,
                     child: ListView.builder(
                       itemCount: transactions.length,
                       itemBuilder: (context, index) {
-                        final transaction =
-                            transactions[index].data() as Map<String, dynamic>;
+                        final transaction = transactions[index].data() as Map<String, dynamic>;
                         final String type = transaction['type'];
                         final double amount = transaction['amount'];
-                        final timestamp =
-                            (transaction['timestamp'] as Timestamp).toDate();
-                        final formattedDate =
-                            DateFormat.yMMMMd().add_jms().format(timestamp);
-                              
-                        // Determine the symbol based on transaction type
+                        final String? category = type == 'load' ? null : transaction['category']; // Exclude category for 'load' transactions
+                        final timestamp = (transaction['timestamp'] as Timestamp).toDate();
+                        final formattedDate = DateFormat.yMMMMd().add_jms().format(timestamp);
+
+                        // Determine the symbol and color based on transaction type
                         String symbol = '';
+                        Color amountColor = Colors.black; // Default color for amount text
                         if (type == 'send') {
                           symbol = '-';
+                          amountColor = Colors.red; // Change color to red for 'send' transactions
                         } else if (type == 'load') {
                           symbol = '+';
+                          amountColor = Colors.green; // Change color to green for 'load' transactions
                         }
-                              
+
                         return Padding(
                           padding: const EdgeInsets.symmetric(vertical: 5),
                           child: ListTile(
                             tileColor: Theme.of(context).colorScheme.tertiary,
-                            title: Text('$symbol ₹$amount'),
-                            subtitle: Text(formattedDate), // Adjust as needed
+                            title: Text(
+                              '$symbol ₹$amount',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: amountColor, // Set the text color based on transaction type
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                if (category != null) Text('Category: $category'), // Conditional rendering of category
+                                Text(formattedDate), // Display date
+                              ],
+                            ),
                           ),
                         );
                       },
